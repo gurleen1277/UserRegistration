36a10030a61d845f0e86de4977fbe3266a47e7da
gurleen1277 <gk184151@gmail.com>
2024-03-26 09:50:46 -0400

    first commit

---
+++ model/userModel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+import {} from 'dotenv/config'
+
+const uri = process.env.MONGO_URI
+
+//const uri = "mongodb+srv://FullStack:Conestoga@cluster0.1ua8had.mongodb.net/CIBC_Users?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(uri).then(()=>{"***************** Connected to Mongodb ***************"})
+.catch((err)=>{console.log(err)})
+
+const userSchema = mongoose.Schema({
+
+    name : {type:String,required:true},
+    email : {type:String,required:true},
+    pwd : {type:String,required:true},
+
+})
+
+const userModel = mongoose.model("CIBC_Visitor",userSchema)
+
+export default userModel
